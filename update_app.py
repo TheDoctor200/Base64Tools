44f155240e0d8c9bc2ee9ec1f43f9dcda79d6f9a
TheDoctor <grafdracula091@gmail.com>
2024-05-17 00:33:46 +0200

    Update 1

--- update_app.py
+++ update_app.py
@@ -3,12 +3,6 @@ import ctypes
 import time
 import sys
 
-# Hiding console window
-SW_HIDE = 0
-GetConsoleWindow = ctypes.windll.kernel32.GetConsoleWindow
-ShowWindow = ctypes.windll.user32.ShowWindow
-ShowWindow(GetConsoleWindow(), SW_HIDE)
-
 # Open link
 url = "https://github.com/TheDoctor200/Base64Tools/releases/latest"
 webbrowser.open(url)
