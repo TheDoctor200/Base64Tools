9b2f335b67a6fc9c4a1d7d33d46b653599e64142
TheDoctor <grafdracula091@gmail.com>
2024-07-08 20:45:29 +0200

    Smart fix in update script

--- update_app.py
+++ update_app.py
@@ -1,3 +1,4 @@
+# imports
 import webbrowser
 import ctypes
 import time
@@ -8,13 +9,12 @@ SW_HIDE = 0
 GetConsoleWindow = ctypes.windll.kernel32.GetConsoleWindow
 ShowWindow = ctypes.windll.user32.ShowWindow
 ShowWindow(GetConsoleWindow(), SW_HIDE)
-
-# Open link
+# Open link to latest version
 url = "https://github.com/TheDoctor200/Base64Tools/releases/latest"
 webbrowser.open(url)
 
-# Wait for 3 seconds
-time.sleep(3)
+# Wait for 2 seconds
+time.sleep(2)
 
 # Close script
 sys.exit()
